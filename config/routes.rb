b1596577cfcc40e3fe3114fda2a3c0539b39b3c0
Dzero-GT <dillongustafson2002@gmail.com>
2022-06-15 18:02:25 -0500

    added navbar partial path and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  #temp about page
+  get "about", to: "about#index"
+
+  #set homepage to root
+  root to: "home#index"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
