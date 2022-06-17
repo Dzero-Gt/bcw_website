18a1e69398bf733ccc033d024f830ce8d1c05891
Dzero-GT <dillongustafson2002@gmail.com>
2022-06-17 18:29:37 -0500

    save before js implementation for menu

---
+++ app/javascript/mobile_listener.js
@@ -0,0 +1,8 @@
+function openMain() {
+  var x = document.getElementById("linksMain");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
