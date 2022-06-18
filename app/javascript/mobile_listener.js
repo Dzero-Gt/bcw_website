02b05107cf54a755b9108cb853d5f57a5f462ac9
Dzero-GT <dillongustafson2002@gmail.com>
2022-06-17 22:01:33 -0500

    first attempt at mobile menu failed

--- app/javascript/mobile_listener.js
+++
@@ -1,8 +0,0 @@
-function openMain() {
-  var x = document.getElementById("linksMain");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
