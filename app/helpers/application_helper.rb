071b71f7bc092fb47fad42935d84fb8337e2b728
Dzero-GT <dillongustafson2002@gmail.com>
2022-06-27 18:30:34 -0500

    added contentful (still must figure out initialization bug)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def contentful
+    @client ||= Contentful::Client.new(
+      access_token: ENV['85QLSeAynKRB0vvA7zOK-DthZkQIvj75Os4C6ClEd4g'],
+      space: ENV['a85lv0q5pkmu'],
+      dynamic_entries: :auto,
+      raise_errors: true
+    )
+  end
 end
